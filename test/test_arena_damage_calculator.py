9e9c7ec43ebda6d025a7bc778bd6626ea1f1069f
Hugo Boueix <hugo.boueix@outlook.com>
2023-02-24 12:26:16 +0100

    :recycle: rename test_compute_damage to be more specific

--- test/test_arena_damage_calculator.py
+++ test/test_arena_damage_calculator.py
@@ -6,7 +6,7 @@ class TestArenaDamageCalculator:
     def setup_method(self, method):
         self.calculator = ArenaDamageCalculator()
 
-    def test_compute_damage(self):
+    def test_compute_damage_return_same_objects(self):
         attackers = [
             Hero(HeroElement.FIRE, 100, 100, 100, 100, 100),
             Hero(HeroElement.WATER, 100, 100, 100, 100, 100),
