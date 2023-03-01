7744f29f39d6dfe60dd31a5314a94baf4d9c413e
Hugo Boueix <hugo.boueix@outlook.com>
2023-03-01 14:50:44 +0100

    :recycle: refacto setup_method for TestArenaDamageCalculator

--- test/test_arena_damage_calculator.py
+++ test/test_arena_damage_calculator.py
@@ -5,33 +5,26 @@ class TestArenaDamageCalculator:
 
     def setup_method(self, method):
         self.calculator = ArenaDamageCalculator()
+        self.attackers = [
+            Hero(HeroElement.FIRE, 100, 100, 100, 100, 100),
+            Hero(HeroElement.WATER, 100, 100, 100, 100, 100),
+            Hero(HeroElement.EARTH, 100, 100, 100, 100, 100)
+        ]
+        self.defenders = [
+            Hero(HeroElement.FIRE, 100, 100, 100, 100, 100),
+            Hero(HeroElement.WATER, 100, 100, 100, 100, 100),
+            Hero(HeroElement.EARTH, 100, 100, 100, 100, 100)
+        ]
 
     def test_compute_damage_return_same_objects(self):
-        attackers = [
-            Hero(HeroElement.FIRE, 100, 100, 100, 100, 100),
-            Hero(HeroElement.WATER, 100, 100, 100, 100, 100),
-            Hero(HeroElement.EARTH, 100, 100, 100, 100, 100)
-        ]
-        defenders = [
-            Hero(HeroElement.FIRE, 100, 100, 100, 100, 100),
-            Hero(HeroElement.WATER, 100, 100, 100, 100, 100),
-            Hero(HeroElement.EARTH, 100, 100, 100, 100, 100)
-        ]
+        updated_defenders = self.calculator.compute_damage(self.attackers[0], self.defenders)
 
-        updated_defenders = self.calculator.compute_damage(attackers[0], defenders)
-
-        assert all([defenders[i] is updated_defenders[i] for i in range(len(defenders))])
+        assert all([self.defenders[i] is updated_defenders[i] for i in range(len(self.defenders))])
 
     def test_get_best_target_if_one_alive(self):
-        attackers = [
-            Hero(HeroElement.FIRE, 100, 100, 100, 100, 100)
-        ]
-        defenders = [
-            Hero(HeroElement.FIRE, 100, 100, 100, 100, lp=0),
-            Hero(HeroElement.WATER, 100, 100, 100, 100, lp=0),
-            Hero(HeroElement.EARTH, 100, 100, 100, 100, lp=100)
-        ]
+        self.defenders[0].lp = 0
+        self.defenders[1].lp = 0
 
-        target = self.calculator.get_best_target(attackers[0], defenders)
+        target = self.calculator.get_best_target(self.attackers[0], self.defenders)
 
-        assert target is defenders[2]
+        assert target is self.defenders[2]
