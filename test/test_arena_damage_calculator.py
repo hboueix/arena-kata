673bc6d1f4f5095b71c065afeceb8a6ff83877b7
Hugo Boueix <hugo.boueix@outlook.com>
2023-03-02 14:04:50 +0100

    :white_check_mark: pass test get_damage if water attacker vs water defender no buff no crit

--- test/test_arena_damage_calculator.py
+++ test/test_arena_damage_calculator.py
@@ -122,3 +122,11 @@ class TestArenaDamageCalculator:
         damage = self.calculator.get_damage(self.attackers[1], self.defenders[0])
 
         assert damage == 117
+
+    def test_get_damage_if_water_attacker_water_defender_no_buff_no_crit(self) -> None:
+        self.attackers[1].crtr = 0
+        self.defenders[1].defense = 150
+
+        damage = self.calculator.get_damage(self.attackers[1], self.defenders[1])
+
+        assert damage == 98
